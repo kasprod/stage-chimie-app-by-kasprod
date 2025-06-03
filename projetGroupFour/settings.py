9f404dc04ea614498e3ccab8c425221fae2ce97a
Alpha Nsimba Kasanji <alphansimbakasandji@gmail.com>
2025-06-03 15:27:07 +0200

    ajaout des fichiers statiques

--- projetGroupFour/settings.py
+++ projetGroupFour/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'projetGroupFour.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+# AUTH_USER_MODEL = 'accounts.CustomUser'
+# LOGIN_URL = 'accounts:login'
+# LOGOUT_URL = 'accounts:logout'
+# LOGIN_REDIRECT_URL = 'app:home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
